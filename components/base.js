73a4dcb3f8f35e8e73785d928c5944379353ed1e
Codehash001 <codehashblockchain@gmail.com>
2023-03-31 20:56:07 +0530

    update

--- components/base.js
+++ components/base.js
@@ -34,11 +34,11 @@ const [toggleState, setToggleState] = useState(1);
       <>
        <div className='w-screen min-h-screen flex flex-col items-center font-Archivo'>  
          <div className='w-screen h-screen bg-bg1 bg-center bg-no-repeat filter brigtness-50'>
-           <div className='flex items-center justify-between filter backdrop-blur-sm bg-black/60 h-[50px] w-full px-16'>
+           <div className='flex items-center justify-between filter backdrop-blur-sm bg-black/60 h-auto w-full px-16 py-4'>
            <div className='font-Kanit font-bold'>Puff Logo.</div>
-           <div className='flex'>
-             <h1 className='text-white font-[18px] mx-5'>Home</h1>
-             <h1 className='text-white font-[18px] mx-5'>Mint</h1>
+           <div className='flex items-center'>
+             <h1 className='text-white font-[18px] mx-5 font-medium'>Home</h1>
+             <h1 className='text-white font-[18px] mx-5 font-medium'>Mint</h1>
              <ConnectButton />
            </div>
              
