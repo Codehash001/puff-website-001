aca1a4ca2c3a56986c88a685cc8d83c925adac26
Codehash001 <codehashblockchain@gmail.com>
2023-03-31 20:50:58 +0530

    update

--- components/base.js
+++ components/base.js
@@ -4,10 +4,15 @@ import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import {Link} from 'react-scroll/modules';
 import Mint from './mint'
 import Overview from './overview'
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig, useAccount } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
 
 export default function Base () {
 
+const account = useAccount()
 
 const[nav, setNav] = useState(false)
 
@@ -29,9 +34,14 @@ const [toggleState, setToggleState] = useState(1);
       <>
        <div className='w-screen min-h-screen flex flex-col items-center'>  
          <div className='w-screen h-screen bg-bg1 bg-center bg-no-repeat filter brigtness-50'>
-           <div className='flex items-center justify-between filter backdrop-blur-sm bg-black/60 h-[50px] w-full'>
-             <h1 className='text-white font-[18px]'>Home</h1>
-             <h1 className='text-white font-[18px]'>Mint</h1>
+           <div className='flex items-center justify-between filter backdrop-blur-sm bg-black/60 h-[50px] w-full px-16'>
+           <div className='font-Kanit font-bold'>Puff Logo.</div>
+           <div>
+             <h1 className='text-white font-[18px] mx-5'>Home</h1>
+             <h1 className='text-white font-[18px] mx-5'>Mint</h1>
+             <ConnectButton />
+           </div>
+             
            </div>
          </div>        
        </div>
