4f8157b7f48e9dd4109766a450ad1fcec650892c
Codehash001 <codehashblockchain@gmail.com>
2023-04-01 22:41:50 +0530

    update

--- components/base.js
+++ components/base.js
@@ -34,7 +34,7 @@ const [toggleState, setToggleState] = useState(1);
       <>
        <div className='w-screen min-h-screen flex flex-col items-center font-Archivo'>  
          <div className='w-screen h-screen bg-bg1 bg-center bg-no-repeat'>
-           <div className='flex items-center justify-between filter backdrop-blur-sm bg-black/60 h-auto w-full px-16 py-4'>
+           <div className='flex items-center justify-between filter backdrop-blur-sm bg-black/40 h-auto w-full px-16 py-4'>
            <div className='font-Kanit font-bold text-lg text-white'>Puff Logo.</div>
            <div className='flex items-center'>
              <h1 className='text-white font-[18px] mx-5 font-medium'>Home</h1>
--- components/mint.js
+++ components/mint.js
@@ -99,7 +99,7 @@ const incrementMintAmount = () => {
   
   return (
   <>
-   <div className='font-Archivo filter backdrop-blur-sm bg-gray-300/50'>
+   <div className='font-Archivo filter backdrop-blur-sm bg-gray-300/30'>
    
     <div className='w-auto h-auto px-6 py-4 flex flex-col justify-center items-center relative '>
     
